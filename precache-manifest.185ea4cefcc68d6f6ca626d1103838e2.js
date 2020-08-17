1cb7dea5658a56dba2f22851a3843cf9dc360ca2
kapathy <katrineiv@hotmail.com>
2020-08-17 21:41:55 +0200

    Updates

---
+++ precache-manifest.185ea4cefcc68d6f6ca626d1103838e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "018eb54ef63b694a120584affc2143b6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c2444925215a24c5ff1d",
+    "url": "./static/css/main.70a789fa.chunk.css"
+  },
+  {
+    "revision": "919f356206d40573e56e",
+    "url": "./static/js/2.b9e6e0d9.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "./static/js/2.b9e6e0d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2444925215a24c5ff1d",
+    "url": "./static/js/main.e59107e5.chunk.js"
+  },
+  {
+    "revision": "c88524ddc0800fb496e3",
+    "url": "./static/js/runtime-main.35a6c3e5.js"
+  },
+  {
+    "revision": "5735b15d41a51aaee399b63632fb66ef",
+    "url": "./static/media/settings.5735b15d.svg"
+  },
+  {
+    "revision": "adb77b2067d574b262cbb8b0b5495670",
+    "url": "./static/media/user.adb77b20.svg"
+  }
+]);
